0174c84a738a57fcf0fca201060e708495f18faf
Tien <tlin@think.edu.au>
2016-03-10 10:14:56 +1100

    [NEW] - Add flow

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
+@flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/components/App';
 
-// App.propTypes = { children: React.PropTypes.string };
-// export default App;
+
 ReactDOM.render( <App title="Select A Life Goal" />, document.getElementById('app'));
-// const HelloMessage = (props) => <h2>Hello {props.name}</h2>;
