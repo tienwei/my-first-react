383f7d1702b01966ff27821d543962e7bd963b92
tienwei <g943942@gmail.com>
2016-03-11 09:05:53 +1100

    [UPDATE] - Put a heading

--- app/components/Bars.jsx
+++ app/components/Bars.jsx
@@ -5,10 +5,18 @@ const {Component} = React;
 
 const Bars = (props) =>
   <div>
-    <h1 title="React Progress Bar"></h1>
+    <h1>{props.title}</h1>
     <Bar title="Bar One"></Bar>
     <Bar title="Bar Two"></Bar>
     <Bar title="Bar Three"></Bar>
+
+    <h2>Controller:</h2>
+    <select>
+      <option value='0'>Select a bar:</option>
+      <option value='1'>Bar One</option>
+      <option value='2'>Bar Two</option>
+      <option value='3'>Bar Three</option>
+    </select>
   </div>;
 
 export default Bars;
