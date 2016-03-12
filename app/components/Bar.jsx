6fa77af2abcb870695882ff5469b405f9beeaece
tienwei <g943942@gmail.com>
2016-03-12 15:32:40 +1100

    [NEW] - Add step option

--- app/components/Bar.jsx
+++ app/components/Bar.jsx
@@ -2,37 +2,32 @@ import React from 'react';
 const {Component} = React;
 
 const divStyle = {
-  border: '1px solid black',
-  height: '50px'
+  border : '1px solid black',
+  height : '50px',
+  position : 'relative'
+};
+
+const midSpanStyle = {
+  position : 'absolute',
+  top: '15px',
+  left: '50%'
 };
 
 class App extends Component {
   constructor(props) {
     super(props);
     // this.fillBar = () => this.fillBar(); For some reason, this will invoke max stack calls
-    this.state = {filled: this.props.initPercent};
-  }
-
-  fillBar() {
-    let newPercent = (this.state.filled < 100)?
-      this.state.filled += 10:
-      this.state.filled;
-    this.setState({filled: newPercent});
-  }
-
-  unfillBar() {
-    let newPercent = (this.state.filled > 0)?
-      this.state.filled -= 10:
-      this.state.filled;
-    this.setState({filled: newPercent});
+    this.state = {filled : this.props.initPercent};
   }
 
   render() {
+    let bgColour = (this.state.filled > 100) ? 'red' : 'blue';
+    let fillArea = (this.state.filled > 100) ? 100 : this.state.filled;
     let spanStyle = {
-      width:`${this.state.filled}%`,
-      background: 'red',
-      height: '100%',
-      display: 'inline-block'
+      width :`${fillArea}%`,
+      background : `${bgColour}`,
+      height : '100%',
+      display : 'inline-block'
     };
 
     return (
@@ -40,8 +35,9 @@ class App extends Component {
         <h2>{this.props.title}</h2>
         <div style={divStyle}>
           <span style={spanStyle}></span>
+          <span style={midSpanStyle}>{`Progress : ${this.state.filled}%`}</span>
         </div>
-        <p>{`Progress: ${this.state.filled}%`}</p>
+
       </div>
     );
   }
@@ -49,7 +45,7 @@ class App extends Component {
 
 
 
-App.propTypes = { initPercent: React.PropTypes.number };
-App.defaultProps = { initPercent: 0 };
+App.propTypes = { initPercent : React.PropTypes.number };
+App.defaultProps = { initPercent : 0 };
 
 export default App;
--- app/components/Bars.jsx
+++ app/components/Bars.jsx
@@ -6,29 +6,37 @@ const {Component} = React;
 class Bars extends Component {
   constructor(props) {
     super(props);
-    this.state = {theBar: ''};
+    this.state = {theBar: '', theStep: 5};
     this.fillBar = this.fillBar.bind(this);
     this.unfillBar = this.unfillBar.bind(this);
     this.selectBar = this.selectBar.bind(this);
+    this.selectStep = this.selectStep.bind(this);
   }
 
   fillBar() {
     const theBar = this.refs[`${this.state.theBar}`];
+    const theStep = this.state.theStep;
     let {filled} = theBar.state;
-    let newPercent = (filled < 100) ? filled += 10 : filled;
+    let newPercent = filled += theStep;
       theBar.setState({filled: newPercent});
   }
 
   unfillBar() {
     const theBar = this.refs[`${this.state.theBar}`];
+    const theStep = this.state.theStep;
     let {filled} = theBar.state;
-    let newPercent = (filled > 10) ? filled -= 10 : filled;
+    let newPercent = (filled > 0) ? filled -= theStep : filled;
       theBar.setState({filled: newPercent});
   }
 
   selectBar(e) {
     // Assign the bar
     this.setState({theBar: e.target.value});
+  }
+
+  selectStep(e) {
+    // Assign the step
+    this.setState({theStep: parseFloat(e.target.value)});
   }
 
   render() {
@@ -45,6 +53,13 @@ class Bars extends Component {
           <option value='bar1'>Bar One</option>
           <option value='bar2'>Bar Two</option>
           <option value='bar3'>Bar Three</option>
+        </select>&nbsp;
+        <select onChange={this.selectStep}>
+          <option value='0'>Select a step:</option>
+          <option value='2.5'>2.5</option>
+          <option value='5'>5</option>
+          <option value='10'>10</option>
+          <option value='20'>20</option>
         </select>&nbsp;
         <button onClick={this.fillBar}>+</button>
         <button onClick={this.unfillBar}>-</button>
