a43e769085589e027aa261436d9964ab7a41249f
Tien <tlin@think.edu.au>
2016-03-18 14:16:13 +1100

    Revert "[NEW] - Update testing package"
    This reverts commit 486e307c65b8b4e71b191f46b56a06909ae20706.
    
    	deleted:    .eslintrc.js
    	modified:   package.json
    	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,37 @@
 var path = require('path');
 var webpack = require('webpack');
-var TapWebpackPlugin = require('tap-webpack-plugin');
 
-module.exports = [
-  // Main bundle config
-  {
-    content: '',
-    entry: [
-      'webpack/hot/dev-server',
-      './main'
+module.exports = {
+  content: '',
+  entry: [
+    'webpack/hot/dev-server',
+    './main.jsx'
+  ],
+  output: {
+    path: path.resolve('build/js/'),
+    publicPath: '/public/assets/js/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+        React: 'react',
+        ReactDOM: 'react-dom',
+        path: 'path'
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        loader: 'babel',
+      }
     ],
-    output: {
-      path: path.resolve('build/js/'),
-      publicPath: '/public/assets/js/',
-      filename: 'bundle.js',
-    },
-    plugins: [
-      new webpack.ProvidePlugin({
-          React: 'react',
-          ReactDOM: 'react-dom',
-          path: 'path'
-      })
-    ],
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          exclude: '/node_modules/',
-          loader: 'babel',
-        }
-      ],
-    },
-    devServer: {
-        contentBase: 'public',
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    }
+  },
+  devServer: {
+      contentBase: 'public',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   }
-
-];
+};
